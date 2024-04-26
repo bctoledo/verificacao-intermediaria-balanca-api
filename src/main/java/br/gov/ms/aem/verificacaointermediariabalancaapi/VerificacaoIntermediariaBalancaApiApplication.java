98677ec4187692541be0ce3a660711e73567704f
Bruno Cesar Gonçalves de Toledo <bruno@inmetroms.gov.br>
2024-04-26 09:52:49 -0400

    Base

---
+++ src/main/java/br/gov/ms/aem/verificacaointermediariabalancaapi/VerificacaoIntermediariaBalancaApiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.ms.aem.verificacaointermediariabalancaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerificacaoIntermediariaBalancaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerificacaoIntermediariaBalancaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/ms/aem/verificacaointermediariabalancaapi/VerificacaoIntermediariaBalancaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.ms.aem.verificacaointermediariabalancaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerificacaoIntermediariaBalancaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
