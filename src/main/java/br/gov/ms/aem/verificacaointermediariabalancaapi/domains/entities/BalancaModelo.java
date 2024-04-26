1cbe5406041f802c25af08fcfb0cbef1f60d619d
Bruno Cesar Gonçalves de Toledo <bruno@inmetroms.gov.br>
2024-04-26 13:00:22 -0400

    Removidos imports nao usados

--- src/main/java/br/gov/ms/aem/verificacaointermediariabalancaapi/domains/entities/BalancaModelo.java
+++ src/main/java/br/gov/ms/aem/verificacaointermediariabalancaapi/domains/entities/BalancaModelo.java
@@ -1,17 +1,15 @@
 package br.gov.ms.aem.verificacaointermediariabalancaapi.domains.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
